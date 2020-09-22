409a794eca5a0a0367224d09d0116a585b9cb581
Jesse <32210049+jessecookedesign@users.noreply.github.com>
2020-09-22 16:57:06 -0500

    LOTS of base updates and additions

---
+++ main.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+            el: '#app',
+            data: {
+                cardData: [
+                    {
+                    id: 1,
+                    imgsrc: "https://i.ibb.co/M6PxVMp/fpf-asset-0001-folkstore.jpg",
+                    imgalt: "Screenshot of folkstore.app",
+                    postedby: "Jesse Cooke",
+                    posteddate: "Sept. 22, 2020",
+                    caption: "Header on folkstore.app. Awesome use of greens together for a modern, artsy aesthetic."
+                    },
+                    {
+                    id: 2,
+                    imgsrc: "https://i.ibb.co/M6PxVMp/fpf-asset-0001-folkstore.jpg",
+                    imgalt: "Screenshot of folkstore.app",
+                    postedby: "Jesse Cooke",
+                    posteddate: "Sept. 22, 2020",
+                    caption: "Header on folkstore.app. Awesome use of greens together for a modern, artsy aesthetic."
+                    }
+                ]
+            }
+        
+        })
