2a862a81f223c6cfecd0f9e9d0d73ad6062e85b1
Jesse <32210049+jessecookedesign@users.noreply.github.com>
2020-09-22 22:30:50 -0500

    Lots of updates

--- main.js
+++ main.js
@@ -8,7 +8,12 @@ var app = new Vue({
                     imgalt: "Screenshot of folkstore.app",
                     postedby: "Jesse Cooke",
                     posteddate: "Sept. 22, 2020",
-                    caption: "Header on folkstore.app. Awesome use of greens together for a modern, artsy aesthetic."
+                    source: "folkstore.app",
+                    sourceurl: "https://www.folkstore.app/",
+                    caption: "Header on folkstore.app. Awesome use of greens together for a modern, artsy aesthetic.",
+                    color1: "#34504a",
+                    color2: "#d2e7d8",
+                    color3: "#ffffff",
                     },
                     {
                     id: 2,
