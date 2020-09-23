2b88e1e41f855f2d99e1a9a2b248947a86b0cc47
Jesse <32210049+jessecookedesign@users.noreply.github.com>
2020-09-22 22:32:04 -0500

    Removed caption

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ var app = new Vue({
                     posteddate: "Sept. 22, 2020",
                     source: "folkstore.app",
                     sourceurl: "https://www.folkstore.app/",
-                    caption: "Header on folkstore.app. Awesome use of greens together for a modern, artsy aesthetic.",
                     color1: "#34504a",
                     color2: "#d2e7d8",
                     color3: "#ffffff",
@@ -19,9 +18,8 @@ var app = new Vue({
                     id: 2,
                     imgsrc: "https://i.ibb.co/M6PxVMp/fpf-asset-0001-folkstore.jpg",
                     imgalt: "Screenshot of folkstore.app",
-                    postedby: "Jesse Cooke",
-                    posteddate: "Sept. 22, 2020",
-                    caption: "Header on folkstore.app. Awesome use of greens together for a modern, artsy aesthetic."
+                    postedby: "Lochie Axon",
+                    posteddate: "Sept. 22, 2020"
                     }
                 ]
             }
