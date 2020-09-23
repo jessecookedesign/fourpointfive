0795851263648a6a32956988cc68968732e069c9
Jesse <32210049+jessecookedesign@users.noreply.github.com>
2020-09-23 00:23:59 -0500

    Added Brid Port

--- main.js
+++ main.js
@@ -12,10 +12,21 @@ var app = new Vue({
                     sourceurl: "https://www.folkstore.app/",
                     color1: "#34504a",
                     color2: "#d2e7d8",
-                    color3: "#ffffff",
+                    color3: "#ffffff"
                     },
                     {
                     id: 2,
+                    imgsrc: "https://i.ibb.co/vH513X5/fpf-asset-0003-bridproperty.jpg",
+                    imgalt: "Screenshot of bridportproperty.com",
+                    postedby: "Lochie Axon",
+                    posteddate: "Sept. 22, 2020",
+                    source: "bridportproperty.com",
+                    sourceurl: "https://bridportproperty.com/",
+                    color1: "#ece8e5",
+                    color2: "#221f20"
+                    },
+                    {
+                    id: 3,
                     imgsrc: "https://i.ibb.co/MBv5xTb/fpf-asset-0002-liquifylochie.jpg",
                     imgalt: "Screenshot of liquify.lochieaxon.com",
                     postedby: "Lochie Axon",
@@ -24,7 +35,7 @@ var app = new Vue({
                     sourceurl: "https://liquify.lochieaxon.com/",
                     color1: "#19191F",
                     color2: "#7A8299",
-                    color3: "#ffffff",
+                    color3: "#ffffff"
                     }
                 ]
             }
